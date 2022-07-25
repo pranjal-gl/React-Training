700710ad279f9eb5540ce08cf7210c3e6b6ecdf1
Pranjal Tripathi <pranjal.tripathi@globallogic.com>
2022-07-25 12:49:33 +0530

    SPA Contact Manager Completed

--- 25-July/SPAContactManager.js
+++ 25-July/SPAContactManager.js
@@ -4,33 +4,41 @@ let uname = document.getElementById('fname');
 let uemail = document.getElementById('email');
 let uphone = document.getElementById('phone');
 
+let updateID;
+function getVal(id) {
+    updateID = id;
+}
+
+// ADDING NEW CONTACT
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
-
-    let data = {
-        id: Date.now(),
-        fname: uname.value,
-        email: uemail.value,
-        phone: uphone.value
-    }
-    const url = 'http://localhost:3001/empDetails';
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
+    if (event.submitter.id == 'add') {
+        let data = {
+            id: Date.now(),
+            fname: uname.value,
+            email: uemail.value,
+            phone: uphone.value
         }
-    }).then((res)=>{
-        return res.json()
-    }).then((res)=>{
-        location.reload();
-        console.log(res);
-    });
+        const url = 'http://localhost:3001/empDetails';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res)=>{
+            return res.json()
+        }).then((res)=>{
+            location.reload();
+            console.log(res);
+        });
+    } else {
+        updateData(updateID);
+    }
+    
 })
 
-
-
-
+// RENDERING ALL CONTACT
 function getContacts() {
     let url = `http://localhost:3001/empDetails`;
     fetch(url).then((res)=> {
@@ -52,8 +60,8 @@ function generateUI(data) {
                     <h3>${element.fname}</h3>
                     <p>${element.email}</p>
                     <p>${element.phone}</p>
-
                     <button onclick="deleteData(${element.id})">Delete</button>
+                    <button onclick="editData(${element.id})">Edit</button>
                 </div>
             </div>
         </div>
@@ -64,6 +72,7 @@ function generateUI(data) {
 
 getContacts();
 
+// DELETE CONTACT
 function deleteData(id) {
     const url = `http://localhost:3001/empDetails/${id}`;
     fetch(url, {
@@ -74,4 +83,41 @@ function deleteData(id) {
         console.log(res);
         location.reload();
     })
+}
+
+// EDIT CONTACT
+function editData(id) {
+    getVal(id);
+    const url = `http://localhost:3001/empDetails/${id}`;
+    fetch(url).then((res)=>{
+        return res.json();
+    }).then((res)=>{
+        // console.log(res);
+        document.getElementById("fname").value = res.fname;
+        document.getElementById("email").value = res.email;
+        document.getElementById("phone").value = res.phone;
+
+        
+    })
+}
+
+function updateData(id) {
+    const url = `http://localhost:3001/empDetails/${id}`;
+    let data = {
+        fname: uname.value,
+        email: uemail.value,
+        phone: uphone.value
+    }
+    fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res)=>{
+        return res.json()
+    }).then((res)=>{
+        location.reload();
+        console.log(res);
+    });
 }
